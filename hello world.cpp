e4b130fc79fc8819cef07ec6fabe62c7376443dc
vlopezpena <37960063+vlopezpena@users.noreply.github.com>
2018-04-04 23:21:16 -0300

    Add files via upload

---
+++ hello world.cpp
@@ -0,0 +1,16 @@
+/*
+Vanessa Guadalupe Lopez Pena
+k1051
+*/
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Hello, World!\n";
+
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
